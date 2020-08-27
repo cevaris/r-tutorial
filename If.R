e52b071e6b47b315641a6ec006553622cf23b060
Adam Cardenas <cevaris@gmail.com>
2020-08-27 03:51:51 +0000

    practiced logical code constructs

---
+++ If.R
@@ -0,0 +1,9 @@
+# modify the value of "var" variable, to dictate what 
+# is printed out via the "if/else" statement
+var <- FALSE
+
+if(var) {
+  "var was TRUE"
+} else {
+  "var was FALSE"
+}
---
+++ While.R
@@ -0,0 +1,15 @@
+
+# print "Hello" forever
+while(TRUE){
+  print("Hello")
+}
+
+# define a integer counter with value "1"
+counter <- 1
+# while the counter is less than 12 do some logic
+while(counter < 12) {
+  # print out the current counter value
+  print(counter)
+  # update the counter variable by 1
+  counter <- counter + 1
+}
