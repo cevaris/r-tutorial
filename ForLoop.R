65916bf2ae968929f676977ede3b1b478c7201fb
Adam Cardenas <cevaris@gmail.com>
2020-08-27 04:00:11 +0000

    for loop

---
+++ ForLoop.R
@@ -0,0 +1,14 @@
+# print "Hello R" 5 times
+for(i in 1:5) { # 1 2 3 4 5
+  print("Hello R")
+}
+
+# print "Hello R" 5 times
+for(i in 5:9) { # 5 6 7 8 9
+  print("Hello R")
+}
+
+# print "Hello, i", for ever number 1 through 5
+for(i in 1:5) {
+  print(paste('Hello', i))
+}
