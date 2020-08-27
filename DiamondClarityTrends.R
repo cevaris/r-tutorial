8687a74c959075df5746b8bf03236634e2073d5f
Adam Cardenas <cevaris@gmail.com>
2020-08-27 03:12:50 +0000

    adding notes

--- DiamondClarityTrends.R
+++ DiamondClarityTrends.R
@@ -1,10 +1,19 @@
-mydata <- read.csv(file.choose())
-
+# to plot data, we need the library ggplot2
+# install ggplot2 and import via library(...) method
 install.packages("ggplot2")
-
 library("ggplot2")
 
-ggplot(data=mydata[mydata$carat<2.5,], aes(x=carat, y=price, color=clarity)) +
+# Download P2-Mispriced-Diamonds.csv from
+# https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/P2-Mispriced-Diamonds.csv
+# read in the CSV (Comma Separated Values) file into the varaible myData
+myData <- read.csv("~/data/P2-Mispriced-Diamonds.csv")
+
+
+# Limit the diamond records to those transactions with <2.5 carat diamonds
+smallerDiamonds = myData[myData$carat<2.5,]
+
+# Using ggplot to graph trends of carat vs price
+ggplot(data=smallerDiamonds, aes(x=carat, y=price, color=clarity)) +
   geom_point(alpha=0.1) +
   geom_smooth()
 
