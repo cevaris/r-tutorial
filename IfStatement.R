ec6a7222ee3d015e1bbc2e515113b4bef35346da
Adam Cardenas <cevaris@gmail.com>
2020-08-27 04:06:59 +0000

    update if statements

--- IfStatement.R
+++ IfStatement.R
@@ -7,3 +7,20 @@ if(var) {
 } else {
   "var was FALSE"
 }
+
+
+
+# reset the value of "x" variable
+rm(x) 
+# choose a random number between 0 and 1, assign to x variable
+x <- rnorm(1)
+
+# use an if statement to choose which message to print out
+if(x > 1) {
+  paste(x, "x was GREATER than 1")
+}  else if(x >= -1) {
+  paste(x, "x was IN BETWEEN -1 and 1")
+} else {
+  paste(x, "x was LESS than -1")
+}
+
