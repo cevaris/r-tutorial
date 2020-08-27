8812469210d18fb567f18f3949ee86b30278a93a
Adam Cardenas <cevaris@gmail.com>
2020-08-27 03:25:28 +0000

    reviewed Types

---
+++ Types.R
@@ -0,0 +1,21 @@
+# this is a comment because it starts with a "#" character
+
+# integer type
+x <- 2L  # x is the variable; 3 is the value
+
+# check the type of a variable
+typeof(x) # type of "x" variable should be "integer"
+
+# double type
+y <- 2.5
+typeof(y) # type of "y" variable should be "double"
+
+# character type
+a <- "h"
+typeof(a) # type of "a" variable should be "character"
+
+# logical type (which can be TRUE or FALSE only)
+q1 <- TRUE
+q2 <- FALSE
+q3 <- !q1
+typeof(q3) # type of "q3" variable should be "logical"
