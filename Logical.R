7299d37ef0331c16d13a298ab90fd0bea703588c
Adam Cardenas <cevaris@gmail.com>
2020-08-27 03:44:46 +0000

    adding Logical and Variables practice

---
+++ Logical.R
@@ -0,0 +1,29 @@
+# checks if left value is less than right value
+4 < 5    # return TRUE
+
+# checks if left value is greater than right value
+10 > 100 # return FALSE
+
+# checks if values are equal
+4 == 5   # returs FALSE
+
+# checks if numbers are not equal
+4 != 5   # returns TRUE
+
+# flip the logical value using "!" character
+TRUE == !TRUE # returns FALSE
+TRUE == TRUE  # returns TRUE
+
+varTrue <- TRUE
+varFalse <- FALSE
+
+# practice using the or "|" operator
+varTrue | varTrue   # returns TRUE
+varFalse | varTrue  # returns TRUE
+varFalse | varFalse # returns FALSE
+
+# practice using the or "&" operator
+varTrue  & varTrue  # returns TRUE
+varFalse & varTrue  # returns FALSE
+varFalse & varFalse # returns FALSE
+
---
+++ Variables.R
@@ -0,0 +1,31 @@
+# Define two example numbers
+A <- 10
+B <- 5
+
+# Sum up the value of A and B, and assign to C variable
+C <- A + B
+# By just typing "C" and executing that line, the value of C is printed out
+C
+
+
+var1 <- 2.5
+var2 <- 4
+
+# divide value of var1 by value of var2
+result <- var1 / var2
+result # should return 0.625
+
+
+# call the builtin function "sqrt", passing in var2
+answer <- sqrt(var2)
+answer # should be 2, because sqrt(4) = 2
+
+
+# Define a string variable, with value "Hello"
+greeting <- "Hello"
+name <- "Bob"
+
+# using the builtin "paste" function we join 
+# greeting and name string values into a single string.
+message <- paste(greeting, name)
+message
